4a32985ba533cd8af4dd7b4aa733e8594f1e451c
Mohab Gabber <mohabgabber0@gmail.com>
2024-05-08 15:15:44 +0300

    implemented vmdetect_data.json mechanism

--- reusables/reusables.go
+++ reusables/reusables.go
@@ -2,6 +2,7 @@ package reusables
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
@@ -11,6 +12,32 @@ import (
 	"golang.org/x/sys/windows/registry"
 )
 
+// * Data struct contains the registry, file and other data that can be used to identify a VM.
+type Data struct {
+	Vbox struct {
+		RegistryKeys []struct {
+			RegPath  string `json:"regPath"`
+			RegKey   string `json:"regKey"`
+			RegValue string `json:"regValue"`
+		} `json:"registryKeys"`
+	} `json:"vbox"`
+}
+
+// * This instance of Data will contain data from the "data.json" file.
+var S Data
+
+// LoadJson will load the file "vmdetect_data.json" into the S instance.
+func (s *Data) LoadJson() {
+	jsonFile := "vmdetect_data.json"
+	if FileAccessible(jsonFile) {
+		f, _ := os.ReadFile(jsonFile)
+		err := json.Unmarshal(f, &s)
+		ErrCheck(err)
+
+	}
+}
+
+// TODO: Implement concurrency to LogWriter and QueryReg
 // QueryReg parses important registry keys which can be used to differentiate between virtual machines and normal operating systems.
 func QueryReg(hive registry.Key, path, key, checkFor string) bool {
 	// Openning the key
@@ -28,6 +55,7 @@ func QueryReg(hive registry.Key, path, key, checkFor string) bool {
 		return false
 	}
 	if strings.Contains(v, checkFor) {
+		LogWriter(fmt.Sprintf("Key: %s With Value: %s", key, v))
 		return true
 	}
 	return false
@@ -41,9 +69,25 @@ func LogWriter(value string) {
 	now := time.Now()
 
 	w := bufio.NewWriter(f)
-	w.WriteString(fmt.Sprintf("%s %s \n", now.Format("02/01/2005 15:04:05 MST"), value))
+	w.WriteString(fmt.Sprintf("%s - %s \n", now.Format("02/01/2005 15:04:05 MST"), value))
 	w.Flush()
+	time.Sleep(time.Second)
 }
+
+// FileAccessible is used to check if a file is accessible or not. mostly utilized to check if the "vmdetect_data.json" exists or not.
+func FileAccessible(path string) bool {
+	if _, err := os.Stat(path); err == nil {
+		LogWriter(fmt.Sprintf("Accessing File: %s Returned: Successful", path))
+		return true
+	} else {
+		LogWriter(fmt.Sprintf("Accessing File: %s Returned: %s", path, err))
+		return false
+	}
+}
+
+// func DownloadFile(url, path string) bool {
+
+// }
 
 // ErrCheck just checks if an error is not nil, in which case it logs the error to stdout.
 func ErrCheck(err error) {
--- vbox/vbox.go
+++ vbox/vbox.go
@@ -7,18 +7,9 @@ import (
 	"golang.org/x/sys/windows/registry"
 )
 
-type vbox struct {
-	registryKeys []struct {
-		regPath  string
-		regKey   string
-		regValue string
-	}
-}
-
-var VboxRegKeys = []string{"HARDWARE\\DEVICEMAP\\Scsi\\Scsi Port 0\\Scsi Bus 0\\Target Id 0\\Logical Unit Id 0", "HARDWARE\\DEVICEMAP\\Scsi\\Scsi Port 1\\Scsi Bus 0\\Target Id 0\\Logical Unit Id 0", "HARDWARE\\DEVICEMAP\\Scsi\\Scsi Port 2\\Scsi Bus 0\\Target Id 0\\Logical Unit Id 0", "SOFTWARE\\VMware, Inc.\\VMware Tools", "HARDWARE\\Description\\System", "SOFTWARE\\Oracle\\VirtualBox Guest Additions", "SYSTEM\\ControlSet001\\Services\\Disk\\Enum", "HARDWARE\\ACPI\\DSDT\\VBOX__", "HARDWARE\\ACPI\\FADT\\VBOX__", "HARDWARE\\ACPI\\RSDT\\VBOX__", "SYSTEM\\ControlSet001\\Services\\VBoxGuest", "SYSTEM\\ControlSet001\\Services\\VBoxMouse", "SYSTEM\\ControlSet001\\Services\\VBoxService", "SYSTEM\\ControlSet001\\Services\\VBoxSF", "SYSTEM\\ControlSet001\\Services\\VBoxVideo"}
-
 func IsVbox() {
-	for _, c := range VboxRegKeys {
-		fmt.Println(re.QueryReg(registry.LOCAL_MACHINE, c, "Identifier", "VMware"))
+	fmt.Println(len(re.S.Vbox.RegistryKeys))
+	for _, c := range re.S.Vbox.RegistryKeys {
+		fmt.Println(re.QueryReg(registry.LOCAL_MACHINE, c.RegPath, c.RegKey, c.RegValue))
 	}
 }
