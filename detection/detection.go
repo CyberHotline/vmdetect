045a6c08a878eb139a44226856dc77064d20ff7e
Mohab Gabber <mohabgabber0@gmail.com>
2024-05-15 20:16:31 +0300

    added verdict and detection of a specific vm platform

--- detection/detection.go
+++ detection/detection.go
@@ -30,30 +30,67 @@ var VB = make(chan bool, 100)
 var VM = make(chan bool, 100)
 var HV = make(chan bool, 100)
 
-var IsVbox bool
-var IsVmware bool
-var IsHyberv bool
-
 func IsVM() {
 	// Loading json data into the S instance
 	S.LoadJson()
 
-	// Starting Check
+	// Start Check
 	VboxCheck()
 	VmwareCheck()
 
-	// Number of operations
+	go func() {
+		G.Wait()
+		close(VB)
+		close(VM)
+		close(HV)
+	}()
+	enumResults()
+}
+func VboxCheck() {
+	for _, c := range S.Vbox.Files {
+		G.Add(1)
+		go FileAccessible(c, VB)
+	}
+	for _, c := range S.Vbox.Processes {
+		G.Add(1)
+		go ProcessEnum(c, VB)
+	}
+	for _, c := range S.Vbox.Services {
+		G.Add(1)
+		go ServiceEnum(c, VB)
+	}
+	for _, c := range S.Vbox.RegistryKeys {
+		G.Add(1)
+		go QueryReg(c.Hive, c.RegPath, c.RegKey, c.RegValue, VB)
+	}
+}
+
+func VmwareCheck() {
+	for _, c := range S.Vmware.Files {
+		G.Add(1)
+		go FileAccessible(c, VM)
+	}
+	for _, c := range S.Vmware.Processes {
+		G.Add(1)
+		go ProcessEnum(c, VM)
+	}
+	for _, c := range S.Vmware.Services {
+		G.Add(1)
+		go ServiceEnum(c, VM)
+	}
+	for _, c := range S.Vmware.RegistryKeys {
+		G.Add(1)
+		go QueryReg(c.Hive, c.RegPath, c.RegKey, c.RegValue, VM)
+	}
+}
+
+func enumResults() {
 	vbno, vmno, hvno := S.countChecks()
 	var (
 		vbco int
 		vmco int
 		hvco int
 	)
-	G.Wait()
-	close(VB)
-	close(VM)
-	close(HV)
-
 	for i := range VB {
 		if i {
 			vbco++
@@ -69,42 +106,19 @@ func IsVM() {
 			hvco++
 		}
 	}
-	fmt.Printf("Results:\n%d of %d\tsuccessful virtualbox checks\n%d of %d\tsuccessful vmware checks\n%d of %d\t successful hyberv checks", vbco, vbno, vmco, vmno, hvco, hvno)
-}
-func VboxCheck() {
-	for _, c := range S.Vbox.Files {
-		G.Add(1)
-		FileAccessible(c, VB)
-	}
-	for _, c := range S.Vbox.Processes {
-		G.Add(1)
-		ProcessEnum(c, VB)
-	}
-	for _, c := range S.Vbox.Services {
-		G.Add(1)
-		ServiceEnum(c, VB)
-	}
-	for _, c := range S.Vbox.RegistryKeys {
-		G.Add(1)
-		QueryReg(c.Hive, c.RegPath, c.RegKey, c.RegValue, VB)
+	LogWriter(fmt.Sprintf("Results:\n%d of %d\tsuccessful virtualbox checks\n%d of %d\tsuccessful vmware checks\n%d of %d\t successful hyberv checks", vbco, vbno, vmco, vmno, hvco, hvno))
+	if float64(vbco) >= (30.0/100.0)*float64(vbno) {
+		verdictPrint("VM - VirtualBox")
+	} else if float64(vmco) >= (30.0/100.0)*float64(vmno) {
+		verdictPrint("VM - VMware")
+	} else if float64(hvco) >= (30.0/100.0)*float64(hvno) {
+		verdictPrint("VM - Hyper-V")
+	} else {
+		verdictPrint("Not VM")
 	}
 }
 
-func VmwareCheck() {
-	for _, c := range S.Vmware.Files {
-		G.Add(1)
-		FileAccessible(c, VM)
-	}
-	for _, c := range S.Vmware.Processes {
-		G.Add(1)
-		ProcessEnum(c, VM)
-	}
-	for _, c := range S.Vmware.Services {
-		G.Add(1)
-		ServiceEnum(c, VM)
-	}
-	for _, c := range S.Vmware.RegistryKeys {
-		G.Add(1)
-		QueryReg(c.Hive, c.RegPath, c.RegKey, c.RegValue, VM)
-	}
+func verdictPrint(text string) {
+	LogWriter(fmt.Sprintf("Verdict: %s", text))
+	fmt.Printf("Verdict: %s", text)
 }
